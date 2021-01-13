6eec2c160b246854e983f8fc1463444a65db3103
Nico Zhu <v-zhumin@microsoft.com>
2021-01-13 15:04:58 +0800

    eidt the property

--- ButtonTypeTest/MyButton.cs
+++ ButtonTypeTest/MyButton.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Data;
@@ -23,8 +24,15 @@ namespace ButtonTypeTest
         public MyButton()
         {
             this.DefaultStyleKey = typeof(MyButton);
+            Loaded += MyButton_Loaded;
 
         }
+
+        private void MyButton_Loaded(object sender, RoutedEventArgs e)
+        {
+            ButtonType = (XButtonType)GetValue(ButtonTypeProperty);
+        }
+
         public XButtonType ButtonType
         {
             get { return (XButtonType)GetValue(ButtonTypeProperty); }
@@ -33,18 +41,19 @@ namespace ButtonTypeTest
 
         // Using a DependencyProperty as the backing store for ButtonType.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ButtonTypeProperty =
-            DependencyProperty.Register("ButtonType", typeof(XButtonType), typeof(MyButton), new PropertyMetadata(0, new PropertyChangedCallback(OnButtonTypeChange)));
+            DependencyProperty.Register("ButtonType", typeof(XButtonType), typeof(MyButton), new PropertyMetadata(null, new PropertyChangedCallback(OnButtonTypeChange)));
 
         private static void OnButtonTypeChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var control = d as MyButton;
+
             switch ((XButtonType)e.NewValue)
             {
                 case XButtonType.TopState:
-                    VisualStateManager.GoToState(control, "TopState", true);
+                    VisualStateManager.GoToState(control, "TopState", false);
                     break;
                 case XButtonType.HiddenTopState:
-                    VisualStateManager.GoToState(control, "HiddenTopState", true);
+                    VisualStateManager.GoToState(control, "HiddenTopState", false);
                     break;
                 default:
                     break;
